9f75f0fa0a194e2563b8e4a4588555d38a76a553
John Ratz <John.Ratz@envieta.com>
2021-03-03 01:43:53 -0500

    All changes to get code to initially compile and run properly!

--- src/HelloWorldClient/HelloWorldClient.cpp
+++ src/HelloWorldClient/HelloWorldClient.cpp
@@ -5,29 +5,35 @@
 
 int main()
 {
-    // initialize the zmq context with a single IO thread
+    // Initialize the zmq context with a single IO thread
     zmq::context_t context{1};
 
-    // construct a REQ (request) socket and connect to interface
+    // Construct a REQ (request) socket and connect to interface
     zmq::socket_t socket{context, zmq::socket_type::req};
     socket.connect("tcp://localhost:5555");
 
-    // set up some static data to send
+    // Set up some static data to send
     const std::string data{"Hello"};
 
     for (auto request_num = 0; request_num < 10; ++request_num)
     {
-        // send the request message
+        // Send the request message
         std::cout << "Sending Hello " << request_num << "..." << std::endl;
         socket.send(zmq::buffer(data), zmq::send_flags::none);
 
-        // wait for reply from server
+        // Wait for reply from server
         zmq::message_t reply{};
-        socket.recv(reply, zmq::recv_flags::none);
 
-        std::cout << "Received " << reply.to_string();
-        std::cout << " (" << request_num << ")";
-        std::cout << std::endl;
+        if (socket.recv(reply, zmq::recv_flags::none))
+        {
+            std::cout << "Received " << reply.to_string();
+            std::cout << " (" << request_num << ")";
+            std::cout << std::endl;
+        }
+        else
+        {
+            std::cout << "FAILED to receive message on socket!" << std::endl;
+        }
     }
 
     return 0;
--- src/HelloWorldServer/HelloWorldServer.cpp
+++ src/HelloWorldServer/HelloWorldServer.cpp
@@ -9,28 +9,34 @@ int main()
 {
     using namespace std::chrono_literals;
 
-    // initialize the zmq context with a single IO thread
+    // Initialize the zmq context with a single IO thread
     zmq::context_t context{1};
 
-    // construct a REP (reply) socket and bind to interface
+    // Construct a REP (reply) socket and bind to interface
     zmq::socket_t socket{context, zmq::socket_type::rep};
     socket.bind("tcp://*:5555");
 
-    // prepare some static data for responses
+    // Prepare some static data for responses
     const std::string data{"World"};
 
     for (;;)
     {
         zmq::message_t request;
 
-        // receive a request from client
-        socket.recv(request, zmq::recv_flags::none);
-        std::cout << "Received " << request.to_string() << std::endl;
+        // Receive a request from client
+        if (socket.recv(request, zmq::recv_flags::none))
+        {
+            std::cout << "Received " << request.to_string() << std::endl;
+        }
+        else
+        {
+            std::cout << "FAILED to receive message on socket!" << std::endl;
+        }
 
-        // simulate work
+        // Simulate work
         std::this_thread::sleep_for(1s);
 
-        // send the reply to the client
+        // Send the reply to the client
         socket.send(zmq::buffer(data), zmq::send_flags::none);
     }
 
